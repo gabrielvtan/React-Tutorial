cc5a5131f121c4b97e5ad160c730c47f3e8cab3c
Gabriel Tan <gabriel.v.tan@gmail.com>
2019-01-17 11:37:36 -0500

    Two way binding of properties

--- src/Section_3/react-complete-guide/src/App.js
+++ src/Section_3/react-complete-guide/src/App.js
@@ -4,6 +4,43 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  // The following only works in 'class extends component'
+  // Unlike "props" which accesses properties from outside a component, "state" accesses properties within a component
+  state = {
+      persons: [
+          {name: 'Gabby', age: 27},
+          {name: 'Person2', age: 100},
+          {name: 'Person3', age: 50}
+      ],
+      otherState: 'some other value'
+  }
+
+  // Handler is used by convention to show that you are not actively calling the method, but using it as an event handler
+  switchNameHandler = (newName) => {
+    //   console.log('Was Clicked!');
+    // DONT DO THIS: this.state.persons[0].name='Gabriel';
+    // this is how you're able to change the current state of variables within a given component
+    this.setState({
+        persons: [
+            {name: newName, age: 27},
+            {name: 'Person2', age: 100},
+            {name: 'Person3', age: 32}
+        ]
+    })
+  }
+
+  // this is for inputing a specific name
+  // event.target.value is how you access the value being inputted into the app
+  nameChangedHandler = (event) => {
+    this.setState({
+        persons: [
+            {name: 'Gabby', age: 27},
+            {name: event.target.value, age: 100},
+            {name: 'Person3', age: 32}
+        ]
+    })
+  }
+
   render() {
     return (
     // Must always use 'className' instead of 'class' for labeling components
@@ -12,10 +49,28 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App </h1>
         <p> This is really working</p>
+        {/* onClick is the event listener */}
+        {/* adding a () to the end of switchNameHandler will cause the method to be execute immediately, so leave it out in this case */}
+
+        {/* you can use the bind(this, list) in order to bind input to the new name (i.e. passing an arugment)*/}
+        {/* <button onClick={this.switchNameHandler.bind(this, 'Gabriel')}>Switch Name</button> */}
+        {/* You may also use the construction below, however, it is not the most efficient way to create the click method */}
+        <button onClick={() =>this.switchNameHandler('Gabriel!!!!')}>Switch Name</button>
         {/* This is customized HTML tag with a self closing back slash */}
-        <Person />
-        <Person />
-        <Person />
+        {/* You can access state by using this.state.persons[n].name */}
+        <Person 
+            name ={this.state.persons[0].name} 
+            age = {this.state.persons[0].age}/>
+        {/* You have the ability to pass functions within the parent component to the children component through the this.switchNameHandler method*/}
+        {/* We can now add changed as a function within the person component */}
+        <Person 
+            name = {this.state.persons[1].name} 
+            age = {this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'GABBBBBBY')}
+            changed={this.nameChangedHandler}>My Hobbies: Racing </Person>
+        <Person 
+            name = {this.state.persons[2].name}
+            age = {this.state.persons[2].age}/>
       </div>
     );
     // createElement takes 3 arguments - This is not the recommended way of creating React components
--- src/Section_3/react-complete-guide/src/Person/Person.js
+++ src/Section_3/react-complete-guide/src/Person/Person.js
@@ -2,9 +2,18 @@ import React from 'react';
 
 // In its simplest form, a component is merely a function which returns a JSX file
 // By convention, the function name should be the same as the component - just lower case
-const person = () => {
+// by convention, props is used to get the properties of a given component
+const person = (props) => {
     // You may add JS functions within text by wrapping the function in curly brackets
-    return <p> I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>
+    return (
+        <div>
+            <p onClick={props.click}> I'm {props.name} and I am {props.age} years old!</p>
+            {/* props.children is special syntax which allows you to access the value between in the opening and closing tags */}
+            <p>{props.children}</p>
+            {/* This updates the value of the name as the input to start */}
+            <input type ="text" onChange={props.changed} value={props.name}/>
+        </div>
+    )
 };
 
 export default person;
